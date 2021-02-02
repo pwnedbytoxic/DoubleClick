f91c166b5593aa1725aaed34eb3471b23faf3599
prodtoxic <47466345+prodbytoxic@users.noreply.github.com>
2021-02-02 16:28:39 -0500

    Update Button.bat

--- Button.bat
+++ Button.bat
@@ -1,9 +1,6 @@
 @echo off
 @setlocal enabledelayedexpansion
 
-:: The Function was created and enhanced by Psi505
-:: You don't have the right to modify it...
-
 ::---- Abbreviations meaning ----::
 :: XBPB = X Button Positin Begin
 :: YBPB = Y Button Positin Begin
